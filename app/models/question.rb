6392e512941cf2df21967485d08db4bb0a04ad2c
Goldie Kedia <namasteyoldie@gmail.com>
2019-06-01 19:29:18 +0530

    list and other functionality connected

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,11 @@
 class Question < ApplicationRecord
+	validates :user_id, presence: true
+
 	belongs_to :user
   	has_many :answers
+  	has_many :question_topics
+  	has_many :topics, through: :question_topics
+
+  	default_scope { order(created_at: :desc) }
+  	scope :topic, ->(topic_id) { joins(:topics).where("topics": { id: topic_id }).group(:id) }
 end
